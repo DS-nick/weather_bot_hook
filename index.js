018b254efed5b412a2a9bacd1fc7c765907cec04
d.kubitskiy <denis.kubicky@gmail.com>
2020-12-01 11:39:46 +0600

    add async func

--- index.js
+++ index.js
@@ -6,7 +6,20 @@ const app = express();
 
 app.use(express.json())
 
-app.get('/', (req, res)=> {
+app.get('/', async (req, res)=> {
+
+    const message_text = "Hello from bot"
+
+    const params = {
+            chat_id: last_message.message.chat.id,
+            text: message_text,
+            parse_mode: 'Markdown'
+            
+        }
+           const tg_res = await fetch(`https://api.telegram.org/bot1444703979:AAEH5sSqoODQz9DM1uIa-fEyQj5iqP8rXqU/sendMessage?${querystring.stringify(params)}`)
+
+
+
     res.send('main work')
 })
 
